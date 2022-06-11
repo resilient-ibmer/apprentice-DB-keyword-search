72a49ab54672e6543179dd186da4a8226c707095
Arvin Fernandez <arvinfernandez@Arvins-Air.lan>
2022-06-10 21:27:17 -0400

    working on getting the assesment criteria window to correctly open and close

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // The main headers where all the text is nested in.
 let [workHeader, compHeader] = document.querySelectorAll('.ibm-show')
+let popupWindow = document.querySelector('assessment-criteria')
 let processes;
 
 // Open the header drop downs
@@ -13,11 +14,36 @@ setTimeout(iterateProcesses, 1000)
 function iterateProcesses(){
     processes = document.querySelectorAll('#processes')
 
-    // Iterate though all the processes
     for(let i = 0; processes[i]; i++){
-        console.log(processes[i], i) 
-        // Collapse info in that process
-        processes[i].firstChild.firstChild.click()      
+
+        setTimeout(() => {
+            // Expand process info
+            processes[i].firstChild.firstChild.click()
+            
+            // If Comp, open Assesment Criteria link
+            if(document.querySelector('.ibm-popup-link')){
+                // Open criteria pop up
+                document.querySelector('.ibm-popup-link').click()
+    
+                // Wait for popup window to finish loading
+                popupWindow.addEventListener("transitionend", traverseCriteriaList)
+    
+            }
+
+        }, 1000)
+
     }
+
+}
+
+function traverseCriteriaList(){
+    let criteriaList = document.querySelector('assessment-criteria ul').children
+
+    for(let i = 0; criteriaList[i]; i++){
+        console.log(criteriaList[i].innerHTML)
+    }
+
+    // Close criteria pop up
+    popupWindow.querySelector('a').click()
 
 }
