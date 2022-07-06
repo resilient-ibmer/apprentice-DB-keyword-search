4dab85caa6b7285e09035431f0a82cb31cedff25
Arvin Fernandez <arvinfernandez@Arvins-Air.lan>
2022-07-06 18:08:03 -0400

    transitioned to using promises resolve() to wait for the popup to load instead of a timeout

--- main.js
+++ main.js
@@ -7,30 +7,32 @@ workHeader.click()
 compHeader.click()
 
 // Giving the process elements a chance to load.
-setTimeout(iterateProcesses, 1000)
+setTimeout(traverseProcesses, 100)
 
+// // Watch for popup window mutations
+// const observer = setUpObserver()
 
-async function iterateProcesses(){
+// Why cant I scroll after the script finishishes???
+
+async function traverseProcesses(){
     let processes = document.querySelectorAll('#processes') 
 
     for(let i = 0; processes[i]; i++){
+        console.log(i)
         // Expand process info
         processes[i].firstChild.firstChild.click()
         // Wait/sleep until expanded drop down loads
         await new Promise(resolve => setTimeout(resolve, 100))
 
         let popupLink = document.querySelector('.ibm-popup-link')
-
         // If Comp, open Assesment Criteria link
         if(popupLink){
-            // Open criteria pop up
             popupLink.click()
+            await new Promise(waitForLoad)
         }
-        // Collaps process info
+        // Collapse process info
         processes[i].firstChild.firstChild.click()
-
     }
-
 }
 
 function traverseCriteriaList(){
@@ -41,32 +43,28 @@ function traverseCriteriaList(){
         console.log(criteriaList[i].innerHTML, criterias)
         criterias++
     }
-
     // Close criteria pop up
     closeBtn.click()
-
 }
 
-// Select the node that will be observed for mutations
-const targetNode = document.querySelector("div.modal.fade")
-// Options for the observer (which mutations to observe)
-const config = { attributes: true, childList: true, subtree: true };
-// Callback function to execute when mutations are observed
-// Thinking about making this an async function???
-const callback = function(mutationList) {
-    for(const mutation of mutationList) {
-        if (mutation.type === "childList"){
-            console.log("OPEN")
-            traverseCriteriaList()
-            break
-        } else if (mutation.attributeName === "style" && mutation.target.ariaHidden === "true"){
-            console.log("CLOSED")
-            // continue iterating through processes
-            break
-        }      
-    }
-};
-// Create an observer instance linked to the callback function
-const observer = new MutationObserver(callback);
-// Start observing the target node for configured mutations
-observer.observe(targetNode, config);
+function waitForLoad(resolve){
+    // Select the node that will be observed for mutations
+    const targetNode = document.querySelector("div.modal.fade")
+    // Options for the observer (which mutations to observe)
+    const config = { attributes: true, childList: true, subtree: true };
+    // Callback function to execute when mutations are observed
+    const callback = function(mutationList) {
+        for(const mutation of mutationList) {
+            if (mutation.type === "childList"){
+                traverseCriteriaList()
+                observer.disconnect()
+                resolve()
+                break
+            }      
+        }
+    };
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback);
+    // Start observing the target node for configured mutations
+    observer.observe(targetNode, config);
+}
