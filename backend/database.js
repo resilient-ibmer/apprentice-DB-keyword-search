13050a09b73fa9009a2bb23b904cbdafdd01e272
Arvin Fernandez <arvinfernandez@Arvins-Air.lan>
2022-07-30 15:10:33 -0400

    succesfully read and write to database using node

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,24 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('keyword_search.db', (error) => {
-    console.log(error)
+    if (error) return console.error(error.message);
+    
+    console.log("Connection Succesfull.");
 });
 
-db.close()
+const sql = "INSERT INTO processes (id, title) VALUES (?,?);"
+const sql2 = "SELECT * FROM processes;"
+
+// db.run(sql, [1, "Apply agile principles and practices"]);
+
+db.all(sql2, (error, rows) => {
+    if (error) return console.error(error.message);
+
+    // This is where we would be looking for those keywords entered on the frontend.
+    rows.forEach( (row) => {
+        console.log(row);
+    });
+});
+
+db.close((error) => {
+    if (error) return console.error(error.message);
+});
