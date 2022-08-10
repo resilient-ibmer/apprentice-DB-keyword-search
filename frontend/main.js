26152ecfed64b5ea471586275443dfafa991fa6b
resilient-ibmer <arvin.fernandez@ibm.com>
2022-08-10 11:13:14 -0400

    MutationObserver works again after browser update && refactoring

--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,6 @@
 // The main headers where all the text is nested in.
 const [workHeader, compHeader] = document.querySelectorAll('.ibm-show')
-let criterias = 0 //133
+let criterias = 0 //133 or 144
 
 // Keeps track of comps so that they can then be sent to backend.
 let data = [];
@@ -12,18 +12,20 @@ compHeader.click()
 // Giving the process elements a chance to load.
 setTimeout(traverseProcesses, 100)
 
+
 async function traverseProcesses(){
     let processes = document.querySelectorAll('#processes') 
 
     // Change this to a forEach or something more readable
     for(let i = 0; i < processes.length; i++){
+
         // Gathering data to be sent to DB
-            // let processPack = {};
-            // let title = processes[i].innerText;
-            // processPack["id"] = processes[i].id;
-            // processPack["title"] = title.substring(0, title.indexOf('\n'));
-            // data.push(processPack);
-        // End of Gathering
+        let title = processes[i].innerText;
+        let processPack = {
+            "id": processes[i].firstChild.id,
+            "title": title.substring(0, title.indexOf('\n')),
+            "criterias": []
+        };
 
         // Expand process info
         processes[i].scrollIntoView()
@@ -35,50 +37,51 @@ async function traverseProcesses(){
         if(popupLink){
             popupLink.click();
             // Wait for modal to open
-            await new Promise(resolve => setTimeout(resolve, 2000));
-            traverseCriteriaList();
+            await new Promise(waitForLoad)
+            traverseCriteriaList(processPack);
+            
             // Wait for modal to close
-            await new Promise(resolve => setTimeout(resolve, 100));
-            // await new Promise(waitForLoad)
+            // await new Promise(resolve => setTimeout(resolve, 100));
         }
+        data.push(processPack);
         // Collapse process info
         processes[i].firstChild.firstChild.click()
     }
+    // Temp fix to the vertical scrolling bar dissapearing after script.
     document.querySelector("body").style['overflow'] = 'auto'
     console.log(data);
 }
 
-function traverseCriteriaList(){
-    let criteriaList = document.querySelector('assessment-criteria ul').children
-    let closeBtn = document.querySelector("a.ibm-close-link.ibm-fright")
+function traverseCriteriaList(processPack){
+    let criteriaList = document.querySelector('assessment-criteria ul').children;
+    let closeBtn = document.querySelector("a.ibm-close-link.ibm-fright");
 
     for(let i = 0; criteriaList[i]; i++){
-        console.log(criteriaList[i].innerHTML, criterias)
-        criterias++
+        processPack["criterias"].push(criteriaList[i].innerHTML);
+        console.log(criteriaList[i].innerHTML, criterias);
+        criterias++;
     }
     // Close criteria pop up
     closeBtn.click()
 }
 
-// function waitForLoad(resolve){
-//     // Select the node that will be observed for mutations
-//     const targetNode = document.querySelector("div.modal.fade")
-//     // Options for the observer (which mutations to observe)
-//     const config = { attributes: true, childList: true, subtree: true };
-//     // Callback function to execute when mutations are observed
-//     const callback = function(mutationList) {
-//         for(const mutation of mutationList) {
-//             console.log("Modal mutation")
-//             if (mutation.type === "childList"){
-//                 traverseCriteriaList()
-//                 observer.disconnect()
-//                 resolve()
-//                 break
-//             }      
-//         }
-//     };
-//     // Create an observer instance linked to the callback function
-//     const observer = new MutationObserver(callback);
-//     // Start observing the target node for configured mutations
-//     observer.observe(targetNode, config);
-// }
+// I could observe the <body>'s classList if I want to close instead
+// Pass in open or close as arguments?
+function waitForLoad(resolve){
+    const targetNode = document.querySelector("div.modal.fade");
+    const config = { attributes: true, childList: true, subtree: true };
+
+    const callback = function(mutationList) {
+        for(const mutation of mutationList) {
+            if (mutation.type === "childList"){
+                // Smelly code with the constant connection and disconnection?
+                observer.disconnect()
+                resolve()
+                break
+            }      
+        }
+    };
+
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
+}
