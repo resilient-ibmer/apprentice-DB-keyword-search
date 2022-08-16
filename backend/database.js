1440e7868878b1b67521790bcf43dec143d456c0
resilient-ibmer <arvin.fernandez@ibm.com>
2022-08-16 12:45:12 -0400

    refactoring

--- backend/database.js
+++ backend/database.js
@@ -6,44 +6,43 @@ let criteriaID = 0;
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('keyword_search.db', (error) => {
     if (error) return console.error(error.message);
-    
     console.log("Connection Succesfull.");
 });
 
 function insertIntoProceses(processes){ 
-    // Check that processes is array and not empty
+    if (isValidArray(processes)){
+        processes.forEach( (process) => {
+            const {id, title, criteria} = process;
+            const sql = "INSERT INTO processes (id, title) VALUES (?,?);"
 
-    processes.forEach( (process) => {
-        const {id, title, criteria} = process;
-        const sql = "INSERT INTO processes (id, title) VALUES (?,?);"
-        
-        db.run(sql, [id, title], (error) => {
-            if (error) return console.log(error);
+            db.run(sql, [id, title], (error) => {
+                if (error) return console.log(error);
+            });
+
+            if (isValidArray(criteria)){
+                insertIntoCriteria(id, criteria);
+            };
         });
-    
-        if (Array.isArray(criteria) && criteria.length){
-            insertIntoCriteria(id, criteria);
-        };
-    });
+    };
 };
 
 function insertIntoCriteria(processID, criteria){
     const sql = "INSERT INTO criteria (id, title, process_id) VALUES (?, ?, ?);"
-
+    
     criteria.forEach( title => {
         criteriaID++;
         db.run(sql, [criteriaID, title, processID], (error) => {
-            if (error) return console.log(error);
+            if (error) return console.log(error.message);
         });
     });
 };
 
 function printTableRows(table){
     const sql2 = "SELECT * FROM processes;"
-
+    
     db.all(sql2, (error, rows) => {
         if (error) return console.error(error.message);
-    
+        
         // This is where we would be looking for those keywords entered on the frontend.
         rows.forEach( (row) => {
             console.log(row);
@@ -51,9 +50,17 @@ function printTableRows(table){
     });
 };
 
+function isValidArray(array){
+    if (Array.isArray(array) && array.length){
+        return true;
+    } else{
+        return false;
+    }
+};
+
 // // It's just good practice \0_0/
 // db.close((error) => {
-//     if (error) return console.error(error.message);
-// });
-
+    //     if (error) return console.error(error.message);
+    // });
+    
 exports.insertIntoProceses = insertIntoProceses;
