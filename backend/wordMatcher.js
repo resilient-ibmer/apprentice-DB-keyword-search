1a68b1d164ace7e982cac2ba254d833a076ae08b
resilient-ibmer <arvin.fernandez@ibm.com>
2022-08-22 11:12:01 -0400

    created functions that will be going through DB processes looking for matches

--- backend/wordMatcher.js
+++ backend/wordMatcher.js
@@ -1,3 +1,30 @@
-function traverseRows(rows){
-    console.log(rows);
-}
+// elementID => queryMatches
+const mactches = {};
+
+function searchProcesses(processes, query){
+    processes.forEach(process => {
+        const {elementID, title, criteria} = process;
+        // replace query with a case insensitive RegEx that searches for query
+        const match = title.search(query);
+
+        if (match){
+            mactches[elementID] = 1;
+        };
+
+        if (criteria.length > 0){
+            searchCriteria(elementID, criteria, query);
+        };
+
+    });
+};
+
+function searchCriteria(processID, criteria, query){
+    criteria.forEach(criterion => {
+        const match = criterion.search(query);
+        
+        if (match){
+            mactches[processID]++;
+        };
+
+    });
+};
