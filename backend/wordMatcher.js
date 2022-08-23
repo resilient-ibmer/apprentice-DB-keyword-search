ccb6af3d475959a1ab6c3f5ecaf574471999848a
resilient-ibmer <arvin.fernandez@ibm.com>
2022-08-23 12:05:50 -0400

    completed matching functionality, wrapped all functions into one function run() because I couldnt leave matches object global since it was being used in different requests. Looks a bit wonky though

--- backend/wordMatcher.js
+++ backend/wordMatcher.js
@@ -1,30 +1,49 @@
-// elementID => queryMatches
-const mactches = {};
 
-function searchProcesses(processes, query){
-    processes.forEach(process => {
-        const {elementID, title, criteria} = process;
-        // replace query with a case insensitive RegEx that searches for query
-        const match = title.search(query);
+function run(processes, query){
+    const matches = {};
+    searchProcesses();
 
-        if (match){
-            mactches[elementID] = 1;
+    function searchProcesses(){
+        processes.forEach(process => {
+            const {elementID, title, criteria} = process;
+            // replace query argument with a case insensitive RegEx that searches for query
+            const match = title.search(query);
+    
+            if (queryMatch(match)){
+                matches[elementID] = 1;
+            };
+            if (criteria.length > 0){
+                searchCriteria(elementID, criteria, query);
+            };
+        });
+    };
+    
+    function searchCriteria(processID, criteria, query){ 
+        criteria.forEach(criterion => {
+            const match = criterion.search(query);
+            
+            if (queryMatch(match)){
+                if (matches[processID]){
+                    matches[processID]++;
+                } else{
+                    matches[processID] = 1;
+                };
+            };
+            
+        });
+    };
+    
+    function queryMatch(match){
+        if (match !== -1){
+            return true;
+        } else{
+            return false;
         };
-
-        if (criteria.length > 0){
-            searchCriteria(elementID, criteria, query);
-        };
-
-    });
+    };
+    
+    return matches;
 };
 
-function searchCriteria(processID, criteria, query){
-    criteria.forEach(criterion => {
-        const match = criterion.search(query);
-        
-        if (match){
-            mactches[processID]++;
-        };
 
-    });
-};
+
+exports.run = run;
