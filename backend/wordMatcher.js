f9c802e25cf15efd526dfa46d5e40cd89b057978
resilient-ibmer <arvin.fernandez@ibm.com>
2022-08-25 11:33:22 -0400

    Unwrapped word matching process from run. Just to keep a consistent style between modules

--- backend/wordMatcher.js
+++ backend/wordMatcher.js
@@ -1,42 +1,40 @@
-function run(processes, matchString){
-    const matches = {};
-    searchProcesses();
+let matches;
 
-    function searchProcesses(){
-        processes.forEach(process => {
-            const {htmlId, title, criteria} = process;
-    
-            if ( queryMatch(title, matchString) ){
-                matches[htmlId] = 1;
-            };
-            if (criteria.length > 0){
-                searchCriteria(htmlId, criteria, matchString);
-            };
-        });
-    };
+function searchProcesses(processes, matchString){
+    // So that matches from previous requests are not sent
+    matches = {};
+    processes.forEach(process => {
+        const {htmlId, title, criteria} = process;
 
-    function searchCriteria(processID, criteria, matchString){ 
-        criteria.forEach(criterion => {
-            if ( queryMatch(criterion, matchString) ){
-                if (matches[processID]){
-                    matches[processID]++;
-                } else{
-                    matches[processID] = 1;
-                };
-            };
-            
-        });
-    };
-    
-    function queryMatch(string, matchString){
-        if (string.toLowerCase().indexOf(matchString) != -1){
-            return true;
-        } else{
-            return false;
-        }
-    };
-
+        if ( queryMatch(title, matchString) ){
+            matches[htmlId] = 1;
+        };
+        if (criteria.length > 0){
+            searchCriteria(htmlId, criteria, matchString);
+        };
+    });
     return matches;
 };
 
-exports.run = run;
+function searchCriteria(processID, criteria, matchString){ 
+    criteria.forEach(criterion => {
+        if ( queryMatch(criterion, matchString) ){
+            if (matches[processID]){
+                matches[processID]++;
+            } else{
+                matches[processID] = 1;
+            };
+        };
+        
+    });
+};
+
+function queryMatch(string, matchString){
+    if (string.toLowerCase().indexOf(matchString) != -1){
+        return true;
+    } else{
+        return false;
+    }
+};
+
+exports.searchProcesses = searchProcesses;
